d6c2a40500986091dc43f1c620d45540fed68df9
SupawichRatchataworrakun <supawich_r@elearning.cmu.ac.th>
2021-09-16 15:24:37 +0700

    6.2

--- src/main/java/se331/lab/rest/organizer/OrganizerController.java
+++ src/main/java/se331/lab/rest/organizer/OrganizerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se331.lab.rest.util.LabMapper;
 
 @RestController
 public class OrganizerController {
@@ -11,6 +12,6 @@ public class OrganizerController {
     OrganizerService organizerService;
     @GetMapping("/organizers")
     ResponseEntity<?> getOrganizers() {
-        return ResponseEntity.ok(organizerService.getAllOrganizer());
+        return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(organizerService.getAllOrganizer()));
     }
 }
